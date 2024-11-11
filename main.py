96e22a3d3d6b08ca65e324970d73d0ecb472f424
taaaf11 <taafuuu@gmail.com>
2024-11-11 22:43:22 +0500

    Control for assignment: initial commit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import flet as ft
 
+from model import Assignment
+from ui.assignment_control import AssignmentControl
+
 
 def main(page: ft.Page):
+    page.add(AssignmentControl(Assignment('hello world', 'hello world description')))
     ...
 
 
