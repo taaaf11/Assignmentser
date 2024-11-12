265f6ef0658ddd30779bec416ae98ad14e6c213c
taaaf11 <taafuuu@gmail.com>
2024-11-12 08:52:02 +0500

    Add deadline info to assignmnet model

--- model.py
+++ model.py
@@ -1,8 +1,18 @@
 from dataclasses import dataclass, field
+from datetime import datetime as dt_class
 
 
 @dataclass
 class Assignment:
     title: str
     description: str
+    deadline: dt_class
     tasks: list[str] = field(default_factory=list)
+
+    @classmethod
+    def from_dict(cls, assignment_data: dict):
+        deadline: dt_class = dt_class.fromisoformat(assignment_data.pop('deadline'))
+        return cls(
+            **assignment_data,
+            deadline=deadline,
+        )
