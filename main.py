b4b231902e5d2efe174d7e505e6d98cd1cdded00
taaaf11 <taafuuu@gmail.com>
2024-11-15 23:14:38 +0500

    Reformat code

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import flet as ft
 from storage import Storage
 from ui.assignment_control import AssignmentControl
 from ui.assignment_create_control import AssignmentCreateControl
-from ui.assignment_details_control import AssignmentDetailsControl
 from ui.assignments_list_column import AssignmentsListControl
 
 
@@ -12,26 +11,31 @@ async def main(page: ft.Page):
 
     async def show_assignment_create_dialog(_):
         async def add_assignment_control_to_page(_):
-            page.assignments_list.add(AssignmentControl(await crt_control.get_assignment()))
+            page.assignments_list.add(
+                AssignmentControl(await crt_control.get_assignment())
+            )
             page.close(dialog)
 
         crt_control = AssignmentCreateControl()
         dialog = ft.AlertDialog(
-                content=crt_control,
-                actions=[ft.IconButton(ft.icons.CHECK, on_click=add_assignment_control_to_page)]
+            content=crt_control,
+            actions=[
+                ft.IconButton(ft.icons.CHECK, on_click=add_assignment_control_to_page)
+            ],
         )
         page.open(dialog)
-
 
     page.scroll = ft.ScrollMode.AUTO
 
     page.fonts = {
-        'Comfortaa': 'assets/fonts/Comfortaa-VariableFont_wght.ttf',
-        'Open-Sans': 'assets/fonts/OpenSans-VariableFont_wdth,wght.ttf',
+        "Comfortaa": "assets/fonts/Comfortaa-VariableFont_wght.ttf",
+        "Open-Sans": "assets/fonts/OpenSans-VariableFont_wdth,wght.ttf",
     }
 
     assignments = await Storage.retrieve_assignments()
-    page.floating_action_button = ft.FloatingActionButton(icon=ft.icons.ADD, on_click=show_assignment_create_dialog)
+    page.floating_action_button = ft.FloatingActionButton(
+        icon=ft.icons.ADD, on_click=show_assignment_create_dialog
+    )
     page.assignments_list = AssignmentsListControl(assignments)
     page.add(page.assignments_list)
 
